5948266863c97a5979c7b469ede8bda3ac065d62
merli <merlin-johannes.hilbert@mni.thm.de>
2021-10-10 17:07:28 +0200

    ended form sign up with state

--- src/components/registration/signup.tsx
+++ src/components/registration/signup.tsx
@@ -1,17 +1,35 @@
 import { TextField, Divider, Button } from '@mui/material';
+import { useState } from 'react';
 
 interface registerData {
-  firstName: string
-  lastName: string
-  userName: string
-  password: string
-  rePassword: string
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  rePassword: string;
 }
 
 function SignUp() {
+  let inputFields: registerData = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    password: '',
+    rePassword: '',
+  };
+  const [inputValues, setInputValues] = useState(inputFields);
+
+  const handleChange = (e: any) => {
+    setInputValues((ev) => ({
+      ...ev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   function submitSignUp(e: any): registerData {
     e.preventDefault();
-    return { firstName: 'ö', lastName: '', userName: '', password: '', rePassword: ''}
+    console.log(inputValues);
+    return inputValues;
   }
 
   return (
@@ -21,45 +39,60 @@ function SignUp() {
       <form onSubmit={submitSignUp} className="authForm">
         <div className="input-group name">
           <TextField
+            name="firstName"
+            onChange={handleChange}
             required
             className="outlined-required"
             label="First Name"
             defaultValue=""
+            autoComplete="given-name"
           />
 
           <TextField
+            name="lastName"
+            onChange={handleChange}
             required
             className="outlined-required"
             label="Last Name"
             defaultValue=""
+            autoComplete="family-name"
           />
         </div>
         <Divider variant="middle" />
         <div className="input-group">
-          <div >
+          <div>
             <TextField
+              name="userName"
+              onChange={handleChange}
               required
               className="outlined-required"
               label="User Name"
               defaultValue=""
+              autoComplete="username"
             />
           </div>
-          <div >
+          <div>
             <TextField
+              name="password"
+              onChange={handleChange}
               required
               className="outlined-required"
               label="Password"
               type="password"
+              autoComplete="new-password"
             />
             <TextField
+              name="rePassword"
+              onChange={handleChange}
               required
               className="outlined-required"
               label="repeat Password"
               type="password"
+              autoComplete="new-password"
             />
           </div>
         </div>
-        <Button type='submit' variant='outlined'>
+        <Button type="submit" variant="outlined">
           REGISTER
         </Button>
       </form>
