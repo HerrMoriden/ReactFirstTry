733fec46d2758dd51e441ea3b2779a837863f685
merli <merlin-johannes.hilbert@mni.thm.de>
2021-10-12 10:02:08 +0200

    wel i tried redux ... maybe delete later

--- src/views/auth/authentication.tsx
+++ src/views/auth/authentication.tsx
@@ -1,10 +1,19 @@
+import { connect } from 'react-redux'
 import './authentication.css';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import Login from '../../components/login/login';
-import SignUp from '../../components/registration/signup';
+import Login from '../../components/auth/login/login';
+import SignUp from '../../components/auth/registration/signup';
+import { useAppSelector } from '../../hooks';
+
+const mapStateToProp = (state: any) => ({
+  loginState: state.auth?.loginState,
+  registerState: state.auth?.registerState
+})
 
 function Authentication() {
   const match = useRouteMatch();
+  let temp = useAppSelector((state) => state.auth?.loginState);
+
   return (
     <div className="container">
       <Switch>
@@ -18,5 +27,7 @@ function Authentication() {
     </div>
   );
 }
+
+connect(mapStateToProp, Authentication)
 
 export default Authentication;
