da449c98bac12e7db4e092c0b85f8910b15249ec
Merlin <merlin.hilbert@mni.thm.de>
2021-10-16 13:15:25 +0200

    wip

--- src/views/auth/authentication.tsx
+++ src/views/auth/authentication.tsx
@@ -4,7 +4,9 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import Login from '../../components/auth/login/login';
 import SignUp from '../../components/auth/registration/signup';
 
+import { getFirebase }
 
+const firebaseInstance = getFirebase();
 export interface RegisterData {
   firstName: string;
   lastName: string;
