3d39d3146bc1f14ba8fc6a6fff5fb0bd424e889c
merli <merlin-johannes.hilbert@mni.thm.de>
2021-10-17 20:03:56 +0200

    the shit works now, the f*cker just doesnt wanna find my .env ... >_<

---
+++ src/firebase.ts
@@ -0,0 +1,56 @@
+import * as firebase from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { resolve } from 'path';
+
+const environment = process.env.NODE_ENV;
+
+console.log('We are in: ' + environment);
+
+const envPath = resolve(__dirname, '..', 'src', 'config', environment + '.env').trim();
+
+console.log( envPath );
+
+require('dotenv').config({ path: './config/development.env'});
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
+
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
+// };
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
+
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = getAuth(app); 
+
+export default app;
