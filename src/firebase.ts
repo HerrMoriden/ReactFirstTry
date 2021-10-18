3e994e5023ac96e188993dc7d3717a7df14a6624
Merlin <merlin.hilbert@mni.thm.de>
2021-10-18 20:22:59 +0200

    Firebase is now finally finding my env.......

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,37 +6,31 @@ const environment = process.env.NODE_ENV;
 
 console.log('We are in: ' + environment);
 
-const envPath = resolve(__dirname, '..', 'src', 'config', environment + '.env').trim();
+const envPath = resolve(process.cwd(), 'config', '.env.' + environment);
 
-console.log( envPath );
+require('dotenv').config({ path: envPath });
 
-require('dotenv').config({ path: './config/development.env'});
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
 
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
 
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
 
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
 
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
 
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
-
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
-
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
-// };
-
-
-
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
+};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
 
 export default app;
